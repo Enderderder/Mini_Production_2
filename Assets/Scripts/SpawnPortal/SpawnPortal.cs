1ad27182a1a39dcf3667bd74e720b0fb8ced6300
Enderderder <richardwulansari@gmail.com>
2018-11-07 10:12:00 +1300

    ~ Added portal script ~ Move CameraRig.cs from legacy folder to main folder

---
+++ Assets/Scripts/SpawnPortal/SpawnPortal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPortal : MonoBehaviour
+{
+
+    public float PoritalSpeed;
+    public float PortalDistance;
+    public bool RotateClockwise;
+
+    public GameObject RotateObjectRef;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Start ()
+    {
+		
+	}
+
+    private void Update ()
+    {
+		
+	}
+}
