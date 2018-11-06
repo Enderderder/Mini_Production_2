98d66f0891c47f91f51cd05fdd5ff6ad7d3163d6
Enderderder <richardwulansari@gmail.com>
2018-11-07 10:58:16 +1300

    ~ Updated spawn portal with rotated around feature

--- Assets/Scripts/SpawnPortal/SpawnPortal.cs
+++ Assets/Scripts/SpawnPortal/SpawnPortal.cs
@@ -4,25 +4,39 @@ using UnityEngine;
 
 public class SpawnPortal : MonoBehaviour
 {
-
-    public float PoritalSpeed;
+    public float PortalSpeed;
     public float PortalDistance;
     public bool RotateClockwise;
+    public Transform RotateObjectRef;
 
-    public GameObject RotateObjectRef;
-
-    private void Awake()
-    {
-        
-    }
+    // Stored value of the original height
+    private float OriginalHeight;
 
     private void Start ()
     {
-		
+        // Store the original height for later use
+        OriginalHeight = this.transform.position.y;
+
+        // Calculate the position of the protal related with the 
+        // orbit reference object while remain the original height
+        Vector3 initialTransform = 
+            (this.transform.position - RotateObjectRef.position).normalized 
+            * PortalDistance + RotateObjectRef.position;
+        initialTransform.y = OriginalHeight;
+        this.transform.position = initialTransform;
 	}
 
     private void Update ()
     {
-		
+        // Base on the rotation direction, change the speed accordingly
+        float rotateSpeed = PortalSpeed;
+        if (RotateClockwise == false)
+        {
+            rotateSpeed = -PortalSpeed;
+        }
+
+        // Apply the transform
+        this.transform.RotateAround(RotateObjectRef.position, 
+            Vector3.up, rotateSpeed * Time.deltaTime);
 	}
 }
