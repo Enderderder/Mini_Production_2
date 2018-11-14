c9fb75745c2eb49444692217391d6aaec98fb6ec
Enderderder <richardwulansari@gmail.com>
2018-11-14 18:29:58 +1300

    ~ Implemented the water heavy attack

--- Assets/Scripts/Player/Spells/WaterHeavySpell.cs
+++ Assets/Scripts/Player/Spells/WaterHeavySpell.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FireHeavySpell : SpecialSpell
+public class WaterHeavySpell : SpecialSpell
 {
     [SerializeField] private float m_DamageTick = 0.5f;
 
